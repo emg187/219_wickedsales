1627d0a9f75d19a2e97ca578066391e717fbea8e
Eric George <geric738@aol.com>
2019-04-05 18:04:31 -0700

    successfully pulled and displayed product details data

--- src/components/products/product_details.js
+++ src/components/products/product_details.js
@@ -1,17 +1,40 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class ProductDetails extends Component {
-    componentDidMount(){
+    state = {
+        details: null
+    }
+
+    async componentDidMount(){
         const {params} = this.props.match;
-        console.log("Fetch product details with id:", params.product_id);
-        //call server to get product details
+        const response = await axios.get(`/api/getproductdetails.php?productId=${params.product_id}`);
+
+        if (response.data.success){
+            this.setState({
+                details: response.data.productInfo
+            });
+        } else {
+            this.setState({
+                details: false
+            });
+        }
     }
 
     render(){
+        const {details} = this.state;
 
+        if (!details===null){
+            return <h1>Loading...</h1>
+        } else if (!details) {
+            return <h1 className="center">No Product Found</h1>
+        }
+      
+        const {description, name} = details;
         return (
             <div className="product-details">
-                <h1 className="center">[Product Name] Details</h1>
+                <h1 className="center">{name}</h1>
+                <p>{description}</p>
             </div>
         );
     }
