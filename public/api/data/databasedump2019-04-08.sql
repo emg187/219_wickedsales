0525a4ede1bc97a8ea838650453666a4a85c0256
Eric George <geric738@aol.com>
2019-04-08 11:21:34 -0700

    updated sql data dump

--- public/api/data/databasedump2019-04-08.sql
+++ public/api/data/databasedump2019-04-08.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 04, 2019 at 08:32 PM
+-- Generation Time: Apr 08, 2019 at 06:20 PM
 -- Server version: 5.7.25
 -- PHP Version: 7.3.1
 
@@ -13,6 +13,34 @@ SET time_zone = "+00:00";
 --
 -- Database: `2.19wickedsales`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carts`
+--
+
+CREATE TABLE `carts` (
+  `id` mediumint(8) UNSIGNED NOT NULL,
+  `item_count` smallint(5) UNSIGNED NOT NULL,
+  `total_price` mediumint(8) UNSIGNED NOT NULL,
+  `created` datetime NOT NULL,
+  `users_id` bigint(20) UNSIGNED NOT NULL,
+  `changed` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart_items`
+--
+
+CREATE TABLE `cart_items` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `products_id` mediumint(8) UNSIGNED NOT NULL,
+  `quantity` smallint(5) UNSIGNED NOT NULL,
+  `carts_id` mediumint(8) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -37,7 +65,9 @@ INSERT INTO `images` (`id`, `products_id`, `url`) VALUES
 (4, 2, 'images/large.jpg'),
 (5, 2, 'images/wicked_shoes_custom_converse.jpg'),
 (6, 3, 'images/wickedbrick.jpg'),
-(7, 3, 'images/wickedbrick2.jpg');
+(7, 3, 'images/wickedbrick2.jpg'),
+(8, 2, 'images/wickedshoes3.jpg'),
+(9, 3, 'images/wickedbrick3.png');
 
 -- --------------------------------------------------------
 
@@ -62,9 +92,41 @@ INSERT INTO `products` (`id`, `name`, `price`, `description`, `misc_details`) VA
 (2, 'Wicked Clogs', 42150, 'These shoes are the noisiest shoes you will ever wear. They rival the decibels of Vivian\'s keyboard.', '{\"availableSizes\":[5,6,7,8,9,10,13,14,17,20],\"colors\":\"red, green, blue, natural\",\"materials\":\"pine, balsa, oak, hemp\"}'),
 (3, 'Wicked Brick', 525, 'It\'s brick, it\'s brick, it\'s big, it\'s bad, it\'s stone. It\'s better than bad, it\'s heavy. Guaranteed to keep the big bad wolf away. ', '{\"height\":\"3 inches\",\"width\":\"3 inches\",\"length\":\"6 inches\",\"colors\":\"red, cherry, maroon, speckled\",\"manufacturer\":\"Riot Brick Studios\",\"weight\":\"8lbs\",\"courseness\":\"very rough\"}\r\n');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(150) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`) VALUES
+(1, 'Eric George ', 'email@email.com');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `carts`
+--
+ALTER TABLE `carts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cart_items`
+--
+ALTER TABLE `cart_items`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `cartproduct` (`carts_id`,`products_id`);
 
 --
 -- Indexes for table `images`
@@ -79,17 +141,41 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `carts`
+--
+ALTER TABLE `carts`
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `cart_items`
+--
+ALTER TABLE `cart_items`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
   MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
