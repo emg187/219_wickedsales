8e6e4ab5b10e6a8b94af80d34f001c6804599792
Eric George <geric738@aol.com>
2019-04-18 17:32:04 -0700

    implemented sign-out with redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,19 @@
+import types from "./types";
+
+
 export function signIn(user){
     console.log("Sign in action creator user data:", user);
 
     return {
-        type: "SIGN_IN", 
-        
+        type: types.SIGN_IN,
+        email: user.email
     }
 }
 
+
+export function signOut(){
+    return {
+        type: types.SIGN_OUT
+    }
+}
 
---
+++ src/components/account/sign_out/sign_out.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react";
+import "./sign_out.scss";
+import {connect} from "react-redux";
+import {signOut} from "../../../actions";
+
+class SignOut extends Component {
+    componentDidMount(){
+        this.props.signOut();
+    }
+
+    render(){
+        return (
+            <div className="sign-out">
+                <div className="sign-out-header center">
+                    <h1 className="center">Thank you for visiting our site</h1>
+                    <h2 className="center">You have been signed out</h2>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default connect(null, {
+    signOut: signOut
+})(SignOut);
+
