ffaf86d10b6c01077f1d0682c2ea644bae5f8a8b
Eric George <geric738@aol.com>
2019-04-25 17:44:32 -0700

    implemented higher order components

---
+++ src/components/account/profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default props=>{
+    return (<div>
+        <h1 className="center">My Profile</h1>
+
+        <ul>
+            <li>Name: Jane Doe</li>
+            <li>Phone: 951-867-5309</li>
+            <li>Email: email@email.com</li>
+        </ul>
+    </div>
+    );
+}
+
